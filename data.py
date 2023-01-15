7d6cd803b3f39a6830bb025a3c4a9b8a9695de1a
Gerisha <75563368+Gerisha@users.noreply.github.com>
2023-01-15 11:24:17 +0530

    Update data.py

--- data.py
+++ data.py
@@ -3,11 +3,11 @@ import pandas as pd
 star_data = pd.read_csv("final1.csv")
 star_rows = star_data[1:]
 
-star_names = star_data["Star Name"]
-star_distances = star_data["Distance (ly)"]
+star_names = star_data["Star_name"]
+star_distances = star_data["Distance"]
 star_masses = star_data["Mass"]
 star_radiuses = star_data["Radius"]
-star_gravity = star_data["Surface Gravity"]
+star_gravity = star_data["Gravity"]
 
 star_list = []
 for index in range(len(star_rows) + 1):
